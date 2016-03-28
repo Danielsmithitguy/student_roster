e46f9cb9c87c84d803abf90a56f1824922da577f
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-03-28 14:10:40 -0600

    adding validations for email
    [#115830807]

--- app/models/students.rb
+++ app/models/students.rb
@@ -2,4 +2,6 @@ class Students < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone_number, presence: true
+  validates :email, format: {with: /[A-Za-z\d.+-@]/, message: "Invalid email"}
+
 end
